6fb094aa80e29a3121c2db6c79006529decc6bde
MM1377 <michael05@knights.ucf.edu>
2021-03-10 00:05:28 -0500

    March 10, 2021 12:05 AM

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const s = new Sidebar({
 /* 
   TODO: 2. Use the rendehhr() method of Sidebar to show HTML-formatted text on console. Results should be wrapped in <ul></ul>
   
-  The first call to render should result in the following:
+  The first call tghghgho render should result in the following:
   <ul><li>Albert Eggstein</li><li>Attila the He</li><li>Attila the Hen</li><li>Dixie Chick</li><li>Gregory Peck</li><li>Mary Poopins</li></ul>
 */
 console.log(s.render()); // modify this line
