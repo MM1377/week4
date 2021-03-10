c1556036fd86f1162f57205c3998471639f06a3f
MM1377 <michael05@knights.ucf.edu>
2021-03-10 00:31:48 -0500

    March 10, 2021 12:31 AM

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,26 +2,26 @@ import Component from './Component.js';
 
 class Sidebar extends Component {
 
-  constructor (props) {
+  constructor(props) {
     super(props);
   }
 
-  // Adds a single string passed in as parameter to array stored in "this.props.menu".
-  addMenuItem (newitem) {
-   this.props.menu.push( (newitem));
-      // TODO: 3. Add a new menu item to the correct variable associated with this class. This involves adding a parameter and changing a class instance variable (props).
-    }
+  // Adds a single string passed in as pdfarameter to array stored in "this.props.menu".
+  addMenuItem(newitem) {
+    this.props.menu.push((newitem));
+    // TODO: 3. Add a new menu item to the correct variable associated with this class. This involves adding a parameter and changing a class instance variable (props).
+  }
 
-  render () {
-   
+  render() {
+
     let responseHTML = '';
 
-    this.props.menu.forEach( (item) => {
-      responseHTML = responseHTML+`<li>${item}</li>`
+    this.props.menu.forEach((item) => {
+      responseHTML = responseHTML + `<li>${item}</li>`
       //console.log """"
       // TODO: 1. Add (concatenate) the value of each item to the variable responseHTML. Each should be wrapped in <li></li> tags.
     });
-    responseHTML =`<ul>${responseHTML}</ul>`
+    responseHTML = `<ul>${responseHTML}</ul>`
 
     return responseHTML;
   }
