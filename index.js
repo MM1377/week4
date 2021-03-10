4d36721204dd4ed7c20ee28b2c50fce73d88397c
MM1377 <michael05@knights.ucf.edu>
2021-03-10 00:04:54 -0500

    March 10, 2021 12:04 AM

---
+++ index.js
@@ -0,0 +1,35 @@
+import Sidebar from './components/Sidebar.js';
+
+import chalk from './node_modules/chalk/source/index.js';
+
+const superred = chalk.bold.red;
+const miniblue = chalk.italic.dim.bgWhite.blue;
+
+const s = new Sidebar({
+  menu: [
+    superred('Albert Eggstein',
+      'Attila the Hen',
+      'Attila the Hen',
+      'Dixie Chick',
+      'Gregory Peck',
+      'Mary Poopins')
+  ]
+});
+
+/* 
+  TODO: 2. Use the rendehhr() method of Sidebar to show HTML-formatted text on console. Results should be wrapped in <ul></ul>
+  
+  The first call to render should result in the following:
+  <ul><li>Albert Eggstein</li><li>Attila the He</li><li>Attila the Hen</li><li>Dixie Chick</li><li>Gregory Peck</li><li>Mary Poopins</li></ul>
+*/
+console.log(s.render()); // modify this line
+s.addMenuItem(miniblue("Oggy Omlette"))
+/* 
+  TODO: 4. Call the new method, addMenuItem, to add a string to the menu item array. Use the string "Oggy Omelette".
+  
+  The second call call of render in the console.log() should result in the following:
+
+  <ul><li>Albert Eggstein</li><li>Attila the Hen</li><li>Attila the Hen</li><li>Dixie Chick</li><li>Gregory Peck</li><li>Mary Poopins</li><li>Oggy Omelette</li></ul>
+*/
+
+console.log(s.render())
---
+++ node_modules/color-convert/README.md
@@ -0,0 +1,6 @@
+# Debugging Assignment 2 with Chalk
+
+[![Build Status](https://travis-ci.org/Qix-/color-convert.svg?branch=master)](https://travis-ci.org/Qix-/color-convert)
+
+
+Integrated Chalk to the existent program in order to style the output according to their defined class
